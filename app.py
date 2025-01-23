6fa077aa8ba37063993d7ac0e995c5c67b7bfd56
Christina Ye <chrye@microsoft.com>
2025-01-23 10:51:57 -0600

    Complete end-to-end working code

--- app.py
+++ app.py
@@ -39,6 +39,8 @@ from azure.search.documents.indexes.models import (
 #Configure Settings
 #region 
 
+print("configuring settings...")
+
 load_dotenv()
 
 # Configure OpenAI settings
@@ -94,17 +96,19 @@ search_client = SearchClient(
 
 #endregion
 
+print("downloading and extracting data...")
+
 embeddings_url = "https://cdn.openai.com/API/examples/data/vector_database_wikipedia_articles_embedded.zip"
 
 # The file is ~700 MB so this will take some time
-#wget.download(embeddings_url)
+wget.download(embeddings_url)
 
-#print("zip file downloaded")
+print("...zip file downloaded")
 
-#with zipfile.ZipFile("vector_database_wikipedia_articles_embedded.zip", "r") as zip_ref:
-#    zip_ref.extractall("data")
+with zipfile.ZipFile("vector_database_wikipedia_articles_embedded.zip", "r") as zip_ref:
+    zip_ref.extractall("data")
 
-#print("zip file extracted")
+print("...zip file extracted")
 
 # Read the CSV file
 article_df = pd.read_csv("data/vector_database_wikipedia_articles_embedded.csv")
@@ -113,7 +117,7 @@ article_df = pd.read_csv("data/vector_database_wikipedia_articles_embedded.csv")
 # Read data with subset of columns
 #article_df = pd.read_csv("data/vector_database_wikipedia_articles_embedded.csv", usecols=usecols)
 
-print("csv content loaded")
+print("...csv content loaded")
 
 # Read vectors from strings back into a list using json.loads
 article_df["title_vector"] = article_df.title_vector.apply(json.loads)
@@ -126,8 +130,10 @@ article_df["vector_id"] = article_df["vector_id"].apply(str)
 #article_df.info()
 #print(article_df.head())
 
+print("...vectors loaded from strings into a list")
+
 # Create an index
-print("creating index")
+print("creating index...")
 
 # Initialize the SearchIndexClient
 index_client = SearchIndexClient(
@@ -177,7 +183,7 @@ vector_search = VectorSearch(
     ],
 )
 
-print("vector search configured")
+print("...vector search configured")
 
 # Configure the semantic search configuration
 semantic_search = SemanticSearch(
@@ -193,7 +199,7 @@ semantic_search = SemanticSearch(
     ]
 )
 
-print("semantic search configured")
+print("...semantic search configured")
 
 # Create the search index with the vector search and semantic search configurations
 index = SearchIndex(
@@ -205,11 +211,11 @@ index = SearchIndex(
 
 # Create or update the index
 result = index_client.create_or_update_index(index)
-print(f"Index <{result.name}> created/updated")
+print(f"...Index <{result.name}> created/updated")
 
 
 # Uploading Data to Azure AI Search Index
-print("Upload Data to Azure AI Search Index")
+print("uploading data to AI search index...")
 
 # Convert the 'id' and 'vector_id' columns to string so one of them can serve as our key field
 article_df["id"] = article_df["id"].astype(str)
@@ -217,7 +223,7 @@ article_df["vector_id"] = article_df["vector_id"].astype(str)
 # Convert the DataFrame to a list of dictionaries
 documents = article_df.to_dict(orient="records")
 
-print(f"total # of records: {len(documents)}. Uploading...")
+print(f"   total # of records: {len(documents)}. uploading...")
 
 # Option 1 - Create a SearchIndexingBufferedSender and upload all documents in a single call 
 # batch_client = SearchIndexingBufferedSender(
@@ -236,31 +242,32 @@ print(f"total # of records: {len(documents)}. Uploading...")
 #     # Clean up resources
 #     batch_client.close()
 
-# # Option 2 - Use search_client to upload one doc at a time
-# for doc in documents:
-#     try:
-#         search_client.upload_documents(documents=[doc])
-#         print(f"Uploaded document with id: {doc['id']}, {doc['vector_id']}")
+# Option 2 - Use search_client to upload one doc at a time
+for doc in documents:
+    try:
+        search_client.upload_documents(documents=[doc])
+        if int(doc['vector_id']) % 1000 == 0:
+            print(f"...uploaded document with id: {doc['id']}, {doc['vector_id']}")
 
-#     except HttpResponseError as hre:
-#         print(f"An error occurred while uploading document with id {doc['id']}: {hre}")
-#     except Exception as e:
-#         print(f"Exception occurred while uploading document with id {doc['id']}: {e}")
+    except HttpResponseError as hre:
+        print(f"An error occurred while uploading document with id {doc['id']}: {hre}")
+    except Exception as e:
+        print(f"Exception occurred while uploading document with id {doc['id']}: {e}")
 
-# #print(f"Uploaded {len(documents)} documents in total")
+#print(f"Uploaded {len(documents)} documents in total")
 
-# # Validate the document count 
-# document_count = search_client.get_document_count()
-# print(f"Document count in the index: {document_count}")
+# Validate the document count 
+document_count = search_client.get_document_count()
+print(f"...Document count in the index: {document_count}")
 
-# # Compare with the total number of documents in your DataFrame
-# total_documents = len(article_df)
-# print(f"Total number of documents in the DataFrame: {total_documents}")
+# Compare with the total number of documents in your DataFrame
+total_documents = len(article_df)
+print(f"...Total number of documents in the DataFrame: {total_documents}")
 
-# if document_count == total_documents:
-#     print("All documents were uploaded correctly.")
-# else:
-#     print("There is a discrepancy in the document count. Please check the logs for any errors.")
+if document_count == total_documents:
+    print("...All documents were uploaded correctly.")
+else:
+    print("WARNING: There is a discrepancy in the document count. Please check the logs for any errors.")
 
 
 # Example function to generate document embedding
@@ -272,14 +279,13 @@ def generate_embeddings(text, model):
     return embedding
 
 first_document_content = documents[0]["text"]
-print(f"Content: {first_document_content[:100]}")
+print(f"...content: {first_document_content[:100]}")
 
-# content_vector = generate_embeddings(first_document_content, deployment)
-print("Content vector generated")
+content_vector = generate_embeddings(first_document_content, deployment)
+print("...content vector generated")
 
 
-print()
-print("==============================")
+print("\n\n==============================\n\n")
 
 
 # Perform a vector similarity search
@@ -287,9 +293,9 @@ print("==============================")
 # Pure Vector Search
 query = "modern art in Europe"
 
-print("====")
-print()
+print("---")
 print(f"Search Test - Vector similarity search: {query}")
+print("---\n")
 
 search_client = SearchClient(search_service_endpoint, index_name, credential)  
 vector_query = VectorizedQuery(vector=generate_embeddings(query, deployment), k_nearest_neighbors=3, fields="content_vector")
@@ -310,9 +316,9 @@ for result in results:
 # Hybrid Search
 query = "Famous battles in Scottish history"  
 
-print("====")
-print()
+print("---")
 print(f"Search Test - Hybrid search: {query}")
+print("---\n")
 
 search_client = SearchClient(search_service_endpoint, index_name, credential)  
 vector_query = VectorizedQuery(vector=generate_embeddings(query, deployment), k_nearest_neighbors=3, fields="content_vector")
@@ -335,9 +341,9 @@ for result in results:
 # Semantic Hybrid Search
 query = "What were the key technological advancements during the Industrial Revolution?"
 
-print("====")
-print()
+print("---")
 print(f"Search Test - Semantic Hybrid search: {query}")
+print("---\n")
 
 search_client = SearchClient(search_service_endpoint, index_name, credential)
 vector_query = VectorizedQuery(
@@ -376,4 +382,7 @@ for result in results:
             print(f"Caption: {caption.highlights}\n")
         else:
             print(f"Caption: {caption.text}\n")
+
+
+print("---- THE END")
 			
