54ff38f587d378a965576e7d8e5f615c3913550c
Christina Ye <45669917+chrye@users.noreply.github.com>
2025-01-31 22:34:16 -0600

    Create write-summary.js

---
+++ .github/workflows/write-summary.js
@@ -0,0 +1,11 @@
+const core = require('@actions/core');
+
+async function run() {
+  const summary = core.summary;
+  summary.addRaw('<h1>My Job Summary</h1>');
+  summary.addRaw('<p>This is a paragraph in the job summary using raw HTML.</p>');
+  summary.addRaw('<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>');
+  await summary.write();
+}
+
+run();
