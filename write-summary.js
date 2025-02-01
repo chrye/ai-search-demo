7adf2712516f2a81d76b0c1d0a76d0366e9bed59
Christina Ye <chrye@microsoft.com>
2025-01-31 23:27:58 -0600

    add async run()

--- write-summary.js
+++ write-summary.js
@@ -1,3 +1,17 @@
+import core from '@actions/core';
+
+//const core = require('@actions/core');
+
 console.log('Hello World');
+
+async function run() {
+    const summary = core.summary;
+    summary.addRaw('<h1>My Job Summary</h1>');
+    summary.addRaw('<p>This is a paragraph in the job summary.</p>');
+    summary.addRaw('<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>');
+    await summary.write();
+}
+
+run();
 
 console.log('Hello World 2');
